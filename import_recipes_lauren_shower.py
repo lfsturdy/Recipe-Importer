7a9d6a771822895b7e2b400dcc0eb0a976623ea0
Lauren Sturdy <lfsturdy@gmail.com>
2018-11-24 14:40:41 -0500

    Fixed markup, Finalized cookbook for Laurie

--- import_recipes_lauren_shower.py
+++ import_recipes_lauren_shower.py
@@ -33,8 +33,8 @@ def main(argv):
     
     #Read in from the spreadsheet. From https://developers.google.com/sheets/quickstart/python
     try:
-        spreadsheetId = '1cVayA8Jw1TWnqn2LpVAV46vIhIpS2hkgXboCtjHldeY' #Id of the spreadsheet with responses
-        rangeName = 'PyCat!A1:L'
+        spreadsheetId = '1-_qfPObOO6UyozFPA4X9dJEyfDS3BlKFj9WuAjxqEVM' #Id of the spreadsheet with responses
+        rangeName = 'PyCat!A2:K'
         result = service.spreadsheets().values().get(
             spreadsheetId=spreadsheetId, range=rangeName).execute()             
     except client.AccessTokenRefreshError:
@@ -56,6 +56,7 @@ def main(argv):
     values = result.get('values', [])
     
     recipelist = {} #Initialize dictionary for recipes by category
+    project = "LIfiles"
     if not values: #Nothing was retrieved from the spreadsheet
         print('No data found.')
     else:
@@ -65,17 +66,18 @@ def main(argv):
             source = fixstring(row[2])
             notes = fixstring(row[3]).replace(u'\n', u" ") #Remove line breaks in notes
             servings = fixstring(row[4])
-            category = fixstring(row[6])
-            ingred = fixstring(row[10])
-            direct = fixstring(row[11])
-            carbs = fixstring(row[5])
-
+            category = fixstring(row[5])
+            ingred = fixstring(row[9])
+            direct = fixstring(row[10])
+            carbs = ""
             subcat = []
-            for cat in row[7:10]: #Since subcategories are in three columns
+            for cat in row[6:9]: #Since subcategories are in three columns
                 if cat: subcat = fixstring(cat)
- 
+                 
             filename = title.replace(" ", "") #Filename is the title minus spaces
-            recipefile = open("files\\"+filename+".tex", "w")
+            filename = filename.replace("\"", "") #Filename also remove quotes
+            filename = filename.replace("/", "") 
+            recipefile = open(project+"\\"+filename+".tex", "w")
 
             ingred = stding(ingred) #Standardize ingredients
             direct = stddir(direct) #Standardize directions (fewer changes than ingredients)
@@ -93,8 +95,8 @@ def main(argv):
             else: recipelist.setdefault(category,[]).append(filename)
         
         #Write the main content file which will call the recipes   
-        cookbookfile = open("files\\recipes.tex", "w")
-        
+        cookbookfile = open(project+"\\recipes.tex", "w")
+        print(recipelist)
         for maincat in categories: 
             if [(k, v) for (k, v) in recipelist.iteritems() if maincat in k]: #If there is a recipe with that main header
                 cookbookfile.write("\\addchap{"+maincat+"}\n")
@@ -106,15 +108,15 @@ def main(argv):
                                 cookbookfile.write("\t\t\\input{" + list + "}\n")
                         
                 else:
-                    print(recipelist)
+                    #print(recipelist)
                     for list in recipelist[maincat]:
                         cookbookfile.write("\t\\input{" + list + "}\n")
         cookbookfile.close()
         
         #Modified from https://github.com/AKuederle/Py-Tex-automation-example/blob/POC/parser.py
-        project = "files"
+        
         build_d = "{}".format(project)
-        out_file = "{}/MasterCookbook".format(build_d)
+        out_file = "{}/LaurenCookbook".format(build_d)
         os.system("pdflatex -output-directory {} {}".format(os.path.realpath(build_d), os.path.realpath(out_file)))
         os.system("makeidx -output-directory {} {}".format(os.path.realpath(build_d), os.path.realpath(out_file)))
         os.system("pdflatex -output-directory {} {}".format(os.path.realpath(build_d), os.path.realpath(out_file)))
@@ -138,17 +140,22 @@ def fixstring(string):
         string = string.replace(u'\xbc', u"\quart") #vulgar quarter
         string = string.replace(u'\xbe', u"\tquart") #vulgar three quarters
         string = string.replace(u'\u2153', u'\\third') #vulgar third (and t)
+        string = string.replace(u'\u2154', u'\\tthird') #vulgar two-thirds
         string = string.replace(u'\u2014', u'---') #em-dash
         string = string.replace(u'_', u'\_') #underscores are protected in LaTeX
         string = string.replace(u'\u223C', u'$\sim$') #LaTeX doesn't deal with tildes
         string = string.replace(u'\u007E', u'$\sim$') #LaTeX doesn't deal with tildes
-        string = string.rstrip() #Remove whitespace at the end--this can cause problems in LaTeX    
+        string = string.rstrip() #Remove whitespace at the end--this can cause problems in LaTeX
+        string = string.replace(u'&', u' and ')
         #I need to indicate links by putting a \url wrapper on it. The code for the regex is based
         #on one I found here: https://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149
         string = re.sub(r'((https?:\/\/)?(www\.)([\da-z-]+)\.([a-z\.]{2,6})([\/\w\\\.-]*))', r'\url{\1}', string)
-        string = re.sub(r'(degree)s?\s([C|F])', r'\1 \2', string) #Remove space between degree symbol and C or F
-    except:
-        print('Help!')
+        string = re.sub(r'(\d\d\d)\s?\\?(degree)?s?((\s?[C|F])|)?', r'\1\\degrees \3', string) #Add space between degree symbol and C or F
+        string = re.sub(r'(\d\d\d)(?!\\)', r'\1\\degree ', string) #Add a degree symbol to any number not followed by a space or slash
+        string = re.sub(r'(\\degrees)(?=( *\.))', r'\\degree ', string) #Correct any extra spaces added before periods
+
+    except IOError, (ErrorNumber, ErrorMessage):
+        print(ErrorMessage)
     return string
 
 def stding(string): #Function to analyze the ingredients and output a properly formatted string
@@ -159,13 +166,14 @@ def stding(string): #Function to analyze the ingredients and output a properly f
     string = string.replace(u'Cups', u'C')
     string = string.replace(u'Cup', u'C')
     string = string.replace(u'cup', u'C')
-    string = string.replace(u' c ', u'C')
+    string = string.replace(u' c ', u' C ')
     string = string.replace(u'tablespoons', u'Tbsp')
     string = string.replace(u'tablespoon', u'Tbsp')
-    string = string.replace(u' T ', u'Tbsp')
+    string = string.replace(u' T ', u' Tbsp ')
+    string = string.replace(u'Tbs ', u'Tbsp')
     string = string.replace(u'teaspoons', u'tsp')
     string = string.replace(u'teaspoon', u'tsp')
-    string = string.replace(u' t ', u'tsp')
+    string = string.replace(u' t ', u' tsp ')
     string = string.replace(u'grams', u'g')
     string = string.replace(u'gram', u'g')
     string = string.replace(u'ounces', u'oz')
@@ -205,8 +213,16 @@ def stddir(string): #Function to analyze the directions and output a properly fo
     string = string.replace(u'2/3', u'\\tthird ')
     string = string.replace(u'1/8', u'\eighth ')
     string = string.replace(u'optional', u'opt.')
-    string = string.replace(u'&', u'\&') #Keep latex happy
     
+
+    # Replace "degrees" with the degree symbol
+    #string = re.sub(r'(?<!\\)\s?degrees?\s(C|F)',r'\\degrees \1', string) #find instances of "degrees F", "degree C", etc. and replace with symbol and C or F
+    #string = re.sub(r'(?<!\\)\s?degrees?\s', r'\\degrees ', string) #If space after, use degrees
+    #string = re.sub(r'(?<!\\)\s?degrees?', r'\\degree ', string)
+    #string = re.sub(r'(\d\d\d) (\\?degrees?)? ?(C|F)', r'\1\\degrees \3', string)
+    #string = re.sub(r'(\d\d\d) (\\?degrees?)?', r'\1\\degrees ', string)
+    #string = re.sub(r'(\d\d\d)(?!(\s|\\))(C|F)', r'\1\\degrees \3', string)
+        
     # The following fixes the spacing between elements
     string = re.sub(r'(\d) (\\)', r'\1\2', string) # Look for numbers followed by spaces by a slash and deletes the space
     string = string.replace(u'\n', u'\n\n') #LaTeX newlines require two (could also be "\\", but newlines are easier to read in the file)
